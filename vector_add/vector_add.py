f3b64513bb07926705a44df1ad119aab4e3e06e6
adream307 <adream307@163.com>
2019-08-11 10:04:11 +0800

    vector add example

---
+++ vector_add/vector_add.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, print_function
+import tvm
+import numpy as np
+
+tgt_host="llvm"
+
+n = tvm.var("n")
+A = tvm.placeholder((n,), name='A')
+B = tvm.placeholder((n,), name='B')
+C = tvm.compute(A.shape, lambda i: A[i] + B[i], name="C")
+print(type(C))
+s = tvm.create_schedule(C.op)
+bx, tx = s[C].split(C.op.axis[0], factor=64)
+
+fadd = tvm.build(s, [A, B, C], 'llvm', target_host=tgt_host, name="myadd")
+print(fadd.get_source())
