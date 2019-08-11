fd717249716bf94f9a50bc3f9e42431a89f19485
adream307 <adream307@163.com>
2019-08-11 16:39:59 +0800

    target c

--- vector_add/vector_add.py
+++ vector_add/vector_add.py
@@ -3,6 +3,8 @@ import tvm
 import numpy as np
 
 tgt_host="llvm"
+# tgt="llvm"
+tgt="c"
 
 n = tvm.var("n")
 A = tvm.placeholder((n,), name='A')
@@ -12,5 +14,17 @@ print(type(C))
 s = tvm.create_schedule(C.op)
 bx, tx = s[C].split(C.op.axis[0], factor=64)
 
-fadd = tvm.build(s, [A, B, C], tvm.target.current_target(), target_host=tgt_host, name="myadd")
-print(fadd.get_source())
+fadd = tvm.build(s, [A, B, C], tgt, target_host=tgt_host, name="myadd")
+
+# print(fadd.get_source())
+
+ctx = tvm.context(tgt, 0)
+
+n = 1024
+a = tvm.nd.array(np.random.uniform(size=n).astype(A.dtype), ctx)
+b = tvm.nd.array(np.random.uniform(size=n).astype(B.dtype), ctx)
+c = tvm.nd.array(np.zeros(n, dtype=C.dtype), ctx)
+fadd(a, b, c)
+tvm.testing.assert_allclose(c.asnumpy(), a.asnumpy() + b.asnumpy())
+
+print("finished.")
